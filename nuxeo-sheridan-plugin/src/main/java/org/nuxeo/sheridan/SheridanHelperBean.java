fb7f83bdbc996717457d7e23a266696f49f8326f
Thibaud Arguillere <targuillere@nuxeo.com>
2016-01-17 13:22:37 -0500

    Work in Progress

--- nuxeo-sheridan-plugin/src/main/java/org/nuxeo/sheridan/SheridanHelperBean.java
+++ nuxeo-sheridan-plugin/src/main/java/org/nuxeo/sheridan/SheridanHelperBean.java
@@ -19,11 +19,13 @@ package org.nuxeo.sheridan;
 import java.io.IOException;
 import java.io.Serializable;
 
+import org.apache.commons.lang.StringUtils;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 
 /**
+ * Bean helper for the Sheridan application
  * 
  * @since 7.10
  */
@@ -32,24 +34,50 @@ import org.jboss.seam.annotations.Scope;
 public class SheridanHelperBean implements Serializable {
 
     private static final long serialVersionUID = -8550491926836699499L;
-    
+
+    /**
+     * Return an S3 Temp Signed Url for the key, using the key id, secret key and bucket set in the configuration.
+     * <p>
+     * An empty key just returns an empty URL in this context (no error)
+     * 
+     * @param inKey
+     * @return
+     * @throws IOException
+     * @since 7.10
+     */
     public String getS3TempSignedUrl(String inKey) throws IOException {
-        
+
         String url = "";
-        
-        S3TempSignedURLBuilder builder = new S3TempSignedURLBuilder();
-        url = builder.build(inKey, 0, null, null);
-        
+
+        if (StringUtils.isNotBlank(inKey)) {
+            S3TempSignedURLBuilder builder = new S3TempSignedURLBuilder();
+            url = builder.build(inKey, 0, null, null);
+        }
+
         return url;
     }
-    
+
+    /**
+     * Return an S3 Temp Signed Url for this key in this bucket, using the key id and secret key set in the configuration.
+     * <p>
+     * An empty key just returns an empty URL in this context (no error)
+     * <p>
+     * if the bucket is empty, the code uses the bucket set in the configuraiton, if any.
+     * 
+     * @param inKey
+     * @return
+     * @throws IOException
+     * @since 7.10
+     */
     public String getS3TempSignedUrl(String inBucket, String inKey) throws IOException {
-        
+
         String url = "";
-        
-        S3TempSignedURLBuilder builder = new S3TempSignedURLBuilder();
-        url = builder.build(inBucket, inKey, 0, null, null);
-        
+
+        if (StringUtils.isNotBlank(inKey)) {
+            S3TempSignedURLBuilder builder = new S3TempSignedURLBuilder();
+            url = builder.build(inBucket, inKey, 0, null, null);
+        }
+
         return url;
     }
 
