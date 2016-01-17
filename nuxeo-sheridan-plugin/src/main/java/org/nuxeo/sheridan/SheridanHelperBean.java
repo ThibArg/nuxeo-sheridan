5a97bc858dbbbac06df62a5ec8ea32b6a27c7e8c
Thibaud Arguillere <targuillere@nuxeo.com>
2016-01-17 12:07:21 -0500

    Work in Progress

--- nuxeo-sheridan-plugin/src/main/java/org/nuxeo/sheridan/SheridanHelperBean.java
+++ nuxeo-sheridan-plugin/src/main/java/org/nuxeo/sheridan/SheridanHelperBean.java
@@ -18,15 +18,10 @@ package org.nuxeo.sheridan;
 
 import java.io.IOException;
 import java.io.Serializable;
-import java.util.HashMap;
 
 import org.jboss.seam.ScopeType;
-import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
-import org.nuxeo.ecm.core.api.CoreSession;
-import org.nuxeo.ecm.core.api.DocumentModel;
-import org.nuxeo.ecm.platform.ui.web.api.NavigationContext;
 
 /**
  * 
